02f5a96f83af03f2627d4482a3d76e35a1bf67a8
Signe Gram Sand <signegramsand@gmail.com>
2021-02-04 13:53:08 +0100

    Blanding

--- Lommeregner/LogikLag/Calculator.cs
+++ Lommeregner/LogikLag/Calculator.cs
@@ -6,24 +6,58 @@ namespace LogikLag
 {
     public class Calculator
     {
+        public double Accumulator { get; private set; }
+
+
+
         public double Add(double a, double b)
         {
-            return (a + b);
+
+            double returnVal = a + b;
+
+
+            Accumulator += returnVal;
+
+            return (returnVal);
         }
 
         public double Subtract(double a, double b)
         {
-            return (a - b);
+            double returnVal = a - b;
+
+            Accumulator += returnVal;
+
+
+            return (returnVal);
         }
 
         public double Multiply(double a, double b)
         {
-            return (a * b);
+            double returnVal = a * b;
+
+            Accumulator += returnVal;
+
+            return (returnVal);
+        }
+
+        public double Divide(double a, double b)
+        {
+            if (b==0)
+            {
+                throw new ArgumentException("Can't divide by 0!");
+            }
+            return a / b;
         }
 
         public double Power(double x, double exp)
         {
-            return (Math.Pow(x, exp));
+
+            double returnVal = Math.Pow(x, exp);
+
+            Accumulator += returnVal;
+
+
+            return (returnVal);
         }
     }
 }
